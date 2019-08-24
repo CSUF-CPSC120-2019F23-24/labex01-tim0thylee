85c792dbdaf402581ab93dd0f4afbbdea2c96816
Timothy Lee <student@cs104u-2f.acad.fullerton.edu>
2019-08-24 13:44:54 -0700

    changed the output to candy questions

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -4,21 +4,22 @@
 
 int main()
 {
-  double hours, rate, pay;
-
+  double bars, rate, pay;
+  // Intro sentence.
+  std::cout << "Welcome to the Earnings Calculator!\n";
   // Get the number of hours worked.
-  std::cout << "How many hours did you work? ";
-  std::cin >> hours;
+  std::cout << "How many candy bars did you sell? ";
+  std::cin >> bars;
 
   // Get the hourly pay rate.
-  std::cout << "How much do you get paid per hour? ";
+  std::cout << "How much does the organization earn for each bar sold? ";
   std::cin >> rate;
 
   // Calculate the pay.
-  pay = hours * rate;
+  pay = bars * rate;
 
   // Display the pay.
-  std::cout << "You have earned $" << pay << std::endl;
+  std::cout << "Congratulations! You have earned $" << pay << " for the organization!" << std::endl;
 
   return 0;
 }
